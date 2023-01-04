af33107e30857e781ade01f5eaad381ab9894e32
Agustin <agustinbatistelli@gmail.com>
2023-01-04 16:29:15 -0300

    init

---
+++ models/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const cors = require("cors");
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.puerto = process.env.PORT;
+    this.usuariosPath = '/api/usuarios';
+
+    // Middlewares
+    this.middlewares();
+
+    // Rutas de mi aplicacion
+    this.routes();
+  }
+
+  middlewares() {
+    // CORS
+    this.app.use(cors());
+
+    // Lectura y parseo del body
+    this.app.use(express.json());
+    
+    // Directorio publico
+    this.app.use( express.static('public'));
+  }
+
+  routes() {
+    this.app.use(this.usuariosPath, require('../routes/usuarios'));
+  }
+
+  listen() {
+    this.app.listen(this.puerto, () => {
+      console.log("Servidor corriendo en puerto",this.puerto);
+    });
+  }
+}
+
+module.exports = Server;
